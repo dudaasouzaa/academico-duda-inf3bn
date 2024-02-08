68ff5ea1b502f071843023a12bb6137e32709cfc
Maria Eduarda Souza Silva <rm88635@estudante.fieb.edu.br>
2024-02-07 22:45:21 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/lip2/academicodudainf3bn/AcademicoDudaInf3bnApplication.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academicodudainf3bn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoDudaInf3bnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoDudaInf3bnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/lip2/academicodudainf3bn/AcademicoDudaInf3bnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academicodudainf3bn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoDudaInf3bnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
